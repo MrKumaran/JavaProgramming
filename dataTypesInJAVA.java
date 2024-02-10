d7db2b242cda17f7c86d740545a85672298f07fa
kumarans <mrkumarancse@gmail.com>
2024-02-11 01:13:30 +0530

    Initial Commit

---
+++ dataTypesInJAVA.java
@@ -0,0 +1,28 @@
+/*
+Some Datatypes in JAVA
+int
+float
+char
+double
+short
+long
+byte
+boolean
+ */
+public class dataTypesInJAVA {
+    public static void main(String[] args) {
+        int Integer = 1; // Numbers including negative numbers
+        String word = "This is string datatypes"; // String is class in java rather than data type which should always enclose withing ""(Double quotes)
+        float MediumDecimal = 2.13F; // Float datatype stores decimal values and Suffix "F" or "f" should be added at end to define float value
+        double LargeDecimal = 3.312312341; // double stores large decimal value if Suffix "F" or "f" is not added it will automatically consider as double and throws error
+        boolean IsStatement = true; // takes value true or false
+        byte smallInteger = 127; // byte datatype can store numbers from -127 to 127
+        System.out.printf("Integer %d\n", Integer);
+        System.out.printf("String %s\n", word);
+        System.out.printf("Float %f\n", MediumDecimal);
+        System.out.printf("Double %f\n", LargeDecimal);
+        System.out.printf("Boolean "+ IsStatement+"\n");
+        System.out.printf("Byte %d", smallInteger);
+    }
+
+}
