8a727bc8ef6787b45160aebd494d53540bc4c5d5
kumarans <mrkumarancse@gmail.com>
2024-04-01 21:17:01 +0530

    Initial

--- client.java
+++ client.java
@@ -4,8 +4,8 @@ import java.net.*;
 public class client {
     public static void main(String[] args) {
         try{
-            Socket s = new Socket("localhost",6666);
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            Socket s = new Socket("localhost",6666); // socket object and passing host IP address and port number.
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream()); // 
             dout.writeUTF("Hello Server");
             dout.flush();
             dout.close();
--- urlConnection.java
+++ urlConnection.java
@@ -1,15 +1,18 @@
 import java.io.*;
 import java.net.*;
+
 public class urlConnection {
-    public static void main(String[] args){
-        try{
-            URL url=new URL("https://velammal.edu.in/");
-            URLConnection urlcon=url.openConnection();
-            InputStream stream=urlcon.getInputStream();
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("https://www.coursera.org/learn/assets-threats-and-vulnerabilities/ungradedWidget/mUW7b/identify-steps-of-the-threat-model-process/");
+            URLConnection urlcon = url.openConnection();
+            InputStream stream = urlcon.getInputStream();
             int i;
-            while((i=stream.read())!=-1){
-                System.out.print((char)i);
+            while ((i = stream.read()) != -1) {
+                System.out.print((char) i);
             }
-        }catch(Exception e){System.out.println(e);}
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }    
